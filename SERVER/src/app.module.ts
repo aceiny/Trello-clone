2f8fb586e2ca55827f49e0b6181994c126100290
aceiny <a_zeraibi@estin.dz>
2024-02-27 17:28:54 +0100

    start defining schemas

--- SERVER/src/app.module.ts
+++ SERVER/src/app.module.ts
@@ -5,11 +5,10 @@ import { JwtModule } from './jwt/jwt.module';
 import { BoardModule } from './board/board.module';
 import { ListModule } from './list/list.module';
 import { CardModule } from './card/card.module';
-import { UserModule } from './user/user.module';
 @Module({
   imports: [
      MongooseModule.forRoot(process.env.MONGO_URI) , 
-     AuthModule, JwtModule, BoardModule, ListModule, CardModule, UserModule
+     AuthModule, JwtModule, BoardModule, ListModule, CardModule
     ],
 })
 export class AppModule {}
