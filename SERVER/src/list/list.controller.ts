14d9b4af3c4abea7025bac8ec95309f9506574f1
aceiny <a_zeraibi@estin.dz>
2024-02-28 17:00:48 +0100

    starting front

--- SERVER/src/list/list.controller.ts
+++ SERVER/src/list/list.controller.ts
@@ -1,16 +1,4 @@
-import {
-  Body,
-  ConflictException,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-  UseGuards,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
+import { Body, ConflictException, Controller, Delete, Get, Param, Post, Put, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ListService } from './list.service';
 import { GetUser } from 'src/jwt/get-user.decorator';
 import { AuthGuard } from '@nestjs/passport';
@@ -19,43 +7,42 @@ import { Board } from 'src/board/board.schema';
 
 @Controller('list')
 export class ListController {
-  constructor(private readonly listService: ListService) {}
-  @Get('/all/:boardId')
-  @UseGuards(AuthGuard())
-  GetAllLists(@GetUser() user, @Param('boardId') boardId: string) {
-    return this.listService.GetAllLists(boardId, user);
-  }
-  @Get('/:id')
-  @UseGuards(AuthGuard())
-  GetList(@Param('id') listId: string) {
-    return this.listService.GetList(listId);
-  }
-  @Post('/:boardId')
-  @UsePipes(ValidationPipe)
-  @UseGuards(AuthGuard())
-  CreateList(@Param('boardId') boardId: string, @Body() list: ListDto) {
-    return this.listService.CreateList(list, boardId);
-  }
-  @Post('/:listId/:boardId')
-  @UseGuards(AuthGuard())
-  ReOrderList(
-    @Param('listId') listId: string,
-    @Param('boardId') boardId: string,
-    @Body('position') position: number,
-  ) {
-    if (!position) {
-      throw new ConflictException('position is required');
+    constructor(
+        private readonly listService: ListService,
+    ) {}
+    @Get('/all/:boardId')
+    @UseGuards(AuthGuard())
+    GetAllLists(@GetUser() user , @Param('boardId') boardId : string){
+        return this.listService.GetAllLists(boardId,user)
     }
-    return this.listService.ReOrderList(boardId, listId, position);
-  }
-  @Put('/:id')
-  @UseGuards(AuthGuard())
-  UpdateList(@Param('id') listId: string, @Body() list: ListDto) {
-    return this.listService.UpdateList(listId, list);
-  }
-  @Delete('/:id')
-  @UseGuards(AuthGuard())
-  DeleteList(@Param('id') listId: string, @Param('boardId') boardId: string) {
-    return this.listService.DeleteList(listId);
-  }
+    @Get('/:id')
+    @UseGuards(AuthGuard())
+    GetList(@Param('id') listId : string){
+        return this.listService.GetList(listId)
+    }
+    @Post('/:boardId')
+    @UsePipes(ValidationPipe)
+    @UseGuards(AuthGuard())
+    CreateList(@Param('boardId') boardId : string , @Body() list : ListDto){
+        return this.listService.CreateList(list , boardId)
+    }
+    @Post('/:listId/:boardId')
+    @UseGuards(AuthGuard())
+    ReOrderList(@Param('listId') listId : string , @Param('boardId') boardId : string , @Body('position') position : number){
+        if(!position) {
+            throw new ConflictException('position is required')
+        }
+        return this.listService.ReOrderList( boardId , listId ,position)
+    }
+    @Put('/:id')
+    @UseGuards(AuthGuard())
+    UpdateList(@Param('id') listId : string , @Body() list : ListDto){
+        return this.listService.UpdateList(listId , list)
+    }
+    @Delete('/:id')
+    @UseGuards(AuthGuard())
+    DeleteList(@Param('id') listId : string , @Param('boardId') boardId : string){
+        return this.listService.DeleteList(listId)
+    }
+
 }
