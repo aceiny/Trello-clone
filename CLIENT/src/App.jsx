9830794f4da616b5d3de77e4fab19e6b8a4e728c
aceiny <a_zeraibi@estin.dz>
2024-03-01 03:37:52 +0100

    fixed bug

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -1,4 +1,3 @@
-import { Browser } from "puppeteer";
 import BoardList from "./componants/Home/BoardList";
 import Sidebar from "./componants/Home/Sidebar";
 import NavBar from "./componants/NavBar";
@@ -11,7 +10,13 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/board" element={<BoardPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
