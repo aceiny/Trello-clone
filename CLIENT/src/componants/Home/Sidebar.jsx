e4197dfe79b12187d396009264375f87efb0614d
yacine <a_zeraibi@estin.dz>
2024-03-20 09:46:09 +0100

    fixed a css problem

--- CLIENT/src/componants/Home/Sidebar.jsx
+++ CLIENT/src/componants/Home/Sidebar.jsx
@@ -4,7 +4,7 @@ import home from '../../assets/icons/home.svg';
 import template from '../../assets/icons/template.svg';
 export default function Sidebar() {
   return (
-    <aside className="  gap-4 px-5 w-[20%] flex flex-col items-start text-[#b6c2cf]">
+    <aside className="  gap-4 px-5 w-[20vw] flex flex-col items-start text-[#b6c2cf]">
       <ul className="flex py-1 w-full flex-col border-b border-[#b6c2cf] ">
         <li className="flex HomeSideBarNav items-center gap-3">
           <img src={dashboard} alt="dashboard" className="w-5 h-5" />
@@ -23,7 +23,7 @@ export default function Sidebar() {
       <article className="w-full flex flex-col gap-2">
         <p className="text-[14px] font-[400] px-3">Workspace</p>
         <div className="HomeSideBarNav items-center justify-start gap-3 flex text-start">
-          <img src="" alt="image" className="rounded-md h-8 w-8" />
+          <img src="https://avatars.githubusercontent.com/u/112300561?v=4" alt="image" className="rounded-md h-8 w-8" />
           <p>Trello Workspace</p>
         </div>
       </article>
